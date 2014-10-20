3c3440d91333c8054c63a78a9a43f5d90ebef2bb
jaychenq <jason0037@163.com>
2014-10-20 13:22:58 +0800

    bugfix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 class TasksController < ApplicationController
-  before_filter :authorize_user!,:except => [:create_question,:question,:answer]
+  before_filter :authorize_user!,:except => [:create_question,:question,:answer,:get_tasks]
   # GET /roles
   # GET /roles.json
   def index
