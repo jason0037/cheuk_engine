d8e994c14f8f9c6b9e8fb2b2fb62bfda1d7d23b5
jaychenq <jason0037@163.com>
2014-10-29 16:21:47 +0800

    bugfix

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,7 @@
 require "pp"
 #encoding: utf-8
 class GuestsController < ApplicationController
-  before_filter :authorize_user!,:except => [:reg_guest,:login]
+  before_filter :authorize_user!,:except => [:reg_guest,:get_guests,:login]
   # GET /roles
   # GET /roles.json
   def index
